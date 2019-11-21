4f1e405894ca45af1c8adb229e1a3417fcc226fc
jiangbw <jbaowei2000@126.com>
2019-11-21 09:36:42 +0800

    整理文件夹

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python_study
 python学习
+1、alien_ship
+打外星人小游戏
+1、learning_log
+学习笔记
--- number.py
+++
@@ -1,8 +0,0 @@
-number = list(range(1,5))
-for e in number:
-	print(e)
-print()
-print("=========列表解析====")
-num_parse = [value**2 for value in range(1,5)]
-for e in num_parse:
-	print(e)
