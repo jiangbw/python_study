a03dd5c080cc968f00d0a3447b48fca7d0f143a1
jiangbw <jbaowei2000@126.com>
2019-11-29 09:25:01 +0800

    Django学习

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,16 +1,10 @@
 """定义learning_logs的URL模式"""
 from django.urls import path
-# from django.urls import path, include
 from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
     # 主页
-    # url(r'^$', views.index, name='index'),
-    #
-    # url(r'^topics/$', views.topics, name='topics'),
-    # url(r'^new_topic/$', views.new_topic, name='new_topic'),
-
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
     path('new_topic/', views.new_topic, name='new_topic'),
